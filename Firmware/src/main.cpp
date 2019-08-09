01c16f03fdf0fc01a9e0082f4e6de313e3b52d2d
Daniel Rojas <github@danielrojas.net>
2019-08-09 13:03:16 +0200

    Four buttons + sleep working

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -13,7 +13,12 @@
 #define BENQ_POWER      0xC40BF
 #define BENQ_ECOBLANK   0xC0CF3
 
-#define PIN_STATUS 13
+#define PIN_STATUS   13
+#define PIN_INTERRUPT 2 // used by attachInterrupt(0,...)
+#define PIN_BTN_VOLDN     5
+#define PIN_BTN_VOLUP     6
+#define PIN_BTN_PWR1      7
+#define PIN_BTN_PWR2      8
 
 IRsend irsend;
 
@@ -37,15 +42,35 @@ void enterSleep() {
 
 void setup()
 {
-  // Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(PIN_STATUS, OUTPUT);
+  pinMode(PIN_BTN_VOLUP, INPUT_PULLUP);
+  pinMode(PIN_BTN_VOLDN, INPUT_PULLUP);
+  pinMode(PIN_BTN_PWR1, INPUT_PULLUP);
+  pinMode(PIN_BTN_PWR2, INPUT_PULLUP);
   digitalWrite(PIN_STATUS, LOW);
 }
 
 void loop() {
   enterSleep();
   digitalWrite(PIN_STATUS, HIGH);
-  irsend.sendNEC(ONKYO_POWER, 32);
+  if        (digitalRead(PIN_BTN_VOLDN) == LOW) {
+    Serial.println("Vol down");
+    irsend.sendNEC(ONKYO_VOLDN, 32);
+  } else if (digitalRead(PIN_BTN_VOLUP) == LOW) {
+    Serial.println("Vol up");
+    irsend.sendNEC(ONKYO_VOLUP, 32);
+  } else if (digitalRead(PIN_BTN_PWR1) == LOW) {
+    Serial.println("Onkyo on/off");
+    irsend.sendNEC(ONKYO_POWER, 32);
+  } else if (digitalRead(PIN_BTN_PWR2) == LOW) {
+    Serial.println("BenQ on/off");
+    irsend.sendNEC(BENQ_POWER, 32);
+  }
+  delay(100);
+  digitalWrite(PIN_STATUS, LOW);
+  // digitalWrite(PIN_STATUS, HIGH);
+  // irsend.sendNEC(ONKYO_POWER, 32);
   // irsend.sendNEC(ONKYO_VOLUP, 32);
 	// delay(200);
   // irsend.sendNEC(ONKYO_VOLUP, 32);
